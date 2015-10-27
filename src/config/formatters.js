5b868cef07984096f1d76d9d5a5b9a69e304a2fd
Eugene Datsky <eugene@datsky.ru>
2015-10-27 15:23:50 +0300

    Use require.resolve to support cases when when formatters installed on the same level as eslint-nibbler (e.g. npm dedupe or npm@3 or just direct formatter usage in the linted module).

--- src/config/formatters.js
+++ src/config/formatters.js
@@ -2,8 +2,6 @@
 These will be used as custom formatters.
  */
 
-import path from 'path';
-
-export var stats = path.resolve(__dirname + '/../../node_modules/eslint-stats/byErrorAndWarning.js');
-export var summary = path.resolve(__dirname + '/../../node_modules/eslint-summary/summary.js');
-export var detailed = path.resolve(__dirname + '/../../node_modules/eslint-friendly-formatter/index.js');
+export var stats = require.resolve('eslint-stats/byErrorAndWarning.js');
+export var summary = require.resolve('eslint-summary/summary.js');
+export var detailed = require.resolve('eslint-friendly-formatter');
