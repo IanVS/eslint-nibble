2e1fd38f870300925ef5c50414cd17284a5a57ad
Sir.Nathan <godlvall2jm@gmail.com>
2023-05-08 01:01:56 -0500

    Fixup failing tests, pull in stripAnsi.

--- src/config/formatters.js
+++ src/config/formatters.js
@@ -3,7 +3,7 @@ These will be used as custom formatters.
  */
 
 const stats = require.resolve('@ianvs/eslint-stats/byErrorAndWarning.js');
-const summary = require.resolve('eslint-summary/summary.js');
+const summary = require.resolve('../summary.js');
 const detailed = require.resolve('eslint-formatter-friendly');
 
 module.exports = {
