c631eb37d1bc35be00ca63e79d8ad611ab103ea4
Ian VanSchooten <ian.vanschooten@gmail.com>
2025-05-27 10:55:15 -0400

    Add @ianvs/prettier-plugin-sort-imports

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,4 +3,12 @@ export default {
   singleQuote: true,
   trailingComma: "es5",
   experimentalTernaries: true,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "^(#api|#assets|#components|#global|#dn-types|#hooks|#mock|#schemas|#screens|#test-setup|#util)(.*)$",
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
+  ],
 };
