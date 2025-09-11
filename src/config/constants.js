03510daaeb98edb719590239e695c8602f1d18e2
Kenton Jacobsen <kenton.jacobsen@gmail.com>
2025-09-11 13:40:59 -0400

    Simplify building of rules for inquirer format

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+const colors = {
+  error: 'red',
+  warning: 'yellow',
+};
+const severities = {
+  1: 'warning',
+  2: 'error',
+};
+const bgColors = {
+  error: 'bgRed',
+  warning: 'bgYellow',
+};
+
+module.exports = { bgColors, colors, severities };
--- src/stats.js
+++ src/stats.js
@@ -1,18 +1,62 @@
-const statsFormatter = require('@ianvs/eslint-stats/byErrorAndWarning.js');
+'use strict';
+const chalk = require('chalk');
+
+const { bgColors, severities } = require('./config/constants.js');
+
+const getBarRatio = (usedColumns, maxResult, maxWidth) => {
+  const maxBarLength = maxWidth - usedColumns;
+  return maxResult <= maxBarLength ? 1 : maxBarLength / maxResult;
+};
+
+function formatRow(ruleText, severity, infractionCount, maxRuleCharacters, maxRuleInfractionCount) {
+  const maxRuleCountCharacters = String(maxRuleInfractionCount).length;
+  const paddedRule = `${ruleText}:`.padEnd(maxRuleCharacters + 2);
+  const paddedCount = chalk.magenta(String(infractionCount).padStart(maxRuleCountCharacters));
+  const maxScreen = process.stdout.columns - 3;
+  const barRatio = getBarRatio(maxRuleCharacters + maxRuleCountCharacters + 3, maxRuleInfractionCount, maxScreen);
+  const barLength = infractionCount * barRatio;
+  const bar = chalk[bgColors[severity]](' '.repeat(barLength));
+  return `${paddedRule}${paddedCount}|${bar}`;
+}
 
 module.exports = function (results) {
-  const stats = statsFormatter(results);
-  // Create an array of inquirer-friendly choices from the stats
-  return stats
-    .split('\n')
-    .filter((stat) => stat) // (filter removes empty stat at end)
-    .map((stat) => {
-      const ruleName = stat.split(':')[0];
+  let maxRuleCharacters = 0;
+  let maxRuleInfractions = 0;
+  const countInfractions = results
+    .flatMap((file) => file.messages)
+    .reduce((acc, message) => {
+      const ruleId = String(message.ruleId);
+      maxRuleCharacters = Math.max(ruleId.length, maxRuleCharacters);
 
-      return {
-        name: stat,
-        value: ruleName,
-        short: ruleName,
+      const severityName = severities[message.severity];
+
+      acc[ruleId] = {
+        ...acc[ruleId],
       };
-    });
+      acc[ruleId][severityName] = (acc[ruleId][severityName] || 0) + 1;
+      maxRuleInfractions = Math.max(acc[ruleId][severityName], maxRuleInfractions);
+      return acc;
+    }, {});
+
+  const severityList = Object.values(severities);
+  const rulesList = Object.keys(countInfractions).sort();
+  return rulesList
+    .flatMap((ruleName) => {
+      return severityList.map((severity) => {
+        if (countInfractions[ruleName][severity]) {
+          return {
+            name: formatRow(
+              ruleName,
+              severity,
+              countInfractions[ruleName][severity],
+              maxRuleCharacters,
+              maxRuleInfractions
+            ),
+            value: ruleName,
+            short: ruleName,
+          };
+        }
+      });
+    })
+    .filter(Boolean);
 };
